49b80c2c6ebcfc44ecc901199612b943b170442a
Allenantony23 <5023101@it.fcrit.ac.in>
2025-03-10 06:56:15 +0000

    components

---
+++ my-react-app/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import Greeting from './components/Greeting';
+
+function App() {
+  return (
+    <div>
+      <Greeting name="Allen" />
+      <Greeting name="Eric" />
+    </div>
+  );
+}
+
+export default App;
