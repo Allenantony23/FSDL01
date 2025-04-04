dc3e422c947bd8df83f09d35f7fd13ba8b7aaafc
Allenantony23 <5023101@it.fcrit.ac.in>
2025-04-04 17:09:30 +0000

    React

---
+++ my-react-app/src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import './App.css'; // This imports the CSS file
+
+function App() {
+  return (
+    <div className="app"> {/* This will use the .app class from CSS */}
+      <h1 className="header">My React App</h1>
+      {/* Other components */}
+    </div>
+  );
+}
+export default App;
+
---
+++ my-react-app/src/Greeting.js
@@ -0,0 +1,10 @@
+function Greeting({ name }) {
+    return (
+      <div className="greeting">
+        <h2>Hello, {name}!</h2>
+        <p>Welcome to our application.</p>
+      </div>
+    );
+  }
+  
+  export default Greeting;
